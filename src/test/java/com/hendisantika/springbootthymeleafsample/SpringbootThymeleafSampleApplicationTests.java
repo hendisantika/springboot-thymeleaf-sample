b17790cccde4e06a2554c27be9f2ac2464c2b832
Hendi Santika <hendisantika@gmail.com>
2020-01-24 06:29:44 +0700

    #1 - Initial Commit

---
+++ src/test/java/com/hendisantika/springbootthymeleafsample/SpringbootThymeleafSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springbootthymeleafsample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootThymeleafSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
