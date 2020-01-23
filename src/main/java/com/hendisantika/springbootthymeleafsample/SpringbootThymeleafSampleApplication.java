4a1fb2ad30c7ea555075e5f823c18f86d98d11ac
Hendi Santika <hendisantika@gmail.com>
2020-01-24 06:32:00 +0700

    #3 - Add @EnableAsync Annotation

--- src/main/java/com/hendisantika/springbootthymeleafsample/SpringbootThymeleafSampleApplication.java
+++ src/main/java/com/hendisantika/springbootthymeleafsample/SpringbootThymeleafSampleApplication.java
@@ -2,8 +2,10 @@ package com.hendisantika.springbootthymeleafsample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class SpringbootThymeleafSampleApplication {
 
 	public static void main(String[] args) {
