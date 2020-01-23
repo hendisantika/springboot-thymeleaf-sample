f5fe4812228f05dcaff2029c7796a33ba2cd14c1
Hendi Santika <hendisantika@gmail.com>
2020-01-24 06:32:41 +0700

    #4 - Add SpringBootServletInitializer

--- src/main/java/com/hendisantika/springbootthymeleafsample/SpringbootThymeleafSampleApplication.java
+++ src/main/java/com/hendisantika/springbootthymeleafsample/SpringbootThymeleafSampleApplication.java
@@ -2,14 +2,21 @@ package com.hendisantika.springbootthymeleafsample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @EnableAsync
-public class SpringbootThymeleafSampleApplication {
+public class SpringbootThymeleafSampleApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootThymeleafSampleApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SpringbootThymeleafSampleApplication.class);
 	}
 
 }
