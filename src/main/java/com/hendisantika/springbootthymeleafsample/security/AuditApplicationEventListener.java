54f6ea7712949ed940b410868022361b32743c89
Hendi Santika <hendisantika@gmail.com>
2020-01-24 06:34:37 +0700

    #7 - Add onAuditEvent in AuditApplicationEventListener

--- src/main/java/com/hendisantika/springbootthymeleafsample/security/AuditApplicationEventListener.java
+++ src/main/java/com/hendisantika/springbootthymeleafsample/security/AuditApplicationEventListener.java
@@ -2,7 +2,11 @@ package com.hendisantika.springbootthymeleafsample.security;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.actuate.audit.AuditEvent;
+import org.springframework.boot.actuate.audit.listener.AuditApplicationEvent;
 import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.context.event.EventListener;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 
 /**
@@ -23,5 +27,23 @@ public class AuditApplicationEventListener {
 
     public AuditApplicationEventListener(ApplicationEventPublisher applicationEventPublisher) {
         this.applicationEventPublisher = applicationEventPublisher;
+    }
+
+    @EventListener(condition = "#event.auditEvent.type != 'CUSTOM_AUDIT_EVENT'")
+    @Async
+    public void onAuditEvent(AuditApplicationEvent event) {
+        AuditEvent actualAuditEvent = event.getAuditEvent();
+
+        LOG.info("On audit application event: timestamp: {}, principal: {}, type: {}, data: {}",
+                actualAuditEvent.getTimestamp(),
+                actualAuditEvent.getPrincipal(),
+                actualAuditEvent.getType(),
+                actualAuditEvent.getData()
+        );
+        applicationEventPublisher.publishEvent(
+                new AuditApplicationEvent(
+                        new AuditEvent(actualAuditEvent.getPrincipal(), "CUSTOM_AUDIT_EVENT")
+                )
+        );
     }
 }
