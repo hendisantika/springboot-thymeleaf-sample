d39527377560853185739d48c43c46a5d866f3e4
Hendi Santika <hendisantika@gmail.com>
2021-05-15 08:23:14 +0700

    #55 - BUg Fix errorn on startup

--- src/main/java/com/hendisantika/springbootthymeleafsample/security/AuditEventListener.java
+++ src/main/java/com/hendisantika/springbootthymeleafsample/security/AuditEventListener.java
@@ -3,7 +3,6 @@ package com.hendisantika.springbootthymeleafsample.security;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.actuate.audit.AuditEvent;
-import org.springframework.boot.actuate.audit.AuditEventRepository;
 import org.springframework.boot.actuate.audit.listener.AbstractAuditListener;
 import org.springframework.stereotype.Component;
 
@@ -21,11 +20,11 @@ public class AuditEventListener extends AbstractAuditListener {
 
     private static final Logger LOG = LoggerFactory.getLogger(AuditEventListener.class);
 
-    private final AuditEventRepository auditEventRepository;
+//    private final AuditEventRepository auditEventRepository;
 
-    public AuditEventListener(AuditEventRepository auditEventRepository) {
-        this.auditEventRepository = auditEventRepository;
-    }
+//    public AuditEventListener(AuditEventRepository auditEventRepository) {
+//        this.auditEventRepository = auditEventRepository;
+//    }
 
     @Override
     protected void onAuditEvent(AuditEvent event) {
@@ -37,7 +36,7 @@ public class AuditEventListener extends AbstractAuditListener {
                 event.getData()
         );
 
-        auditEventRepository.add(event);
+//        auditEventRepository.add(event);
     }
 
 }
